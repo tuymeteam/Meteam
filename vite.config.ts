ff9b81bc6b2abb2a5856b5deeebf26e082ba9250
tuymeteam <tranxuantuy2112@gmail.com>
2026-06-13 13:46:51 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    <script type="module" src="/index.tsx"></script>
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
